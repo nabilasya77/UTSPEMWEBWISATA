09932aeb4f9e155120dc61e1b16004f508d754da
nabilasya77 <nabilacheisya_7@student.uns.ac.id>
2026-04-25 13:08:47 +0700

    update

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,33 +1,12 @@
 <?php
-// 1. Data dari TiDB Cloud (WAJIB DIISI)
-$host = ''; 
-$port = 3306                                       
-$user = '';                                    
-$pass = '';                                   
-$db   = 'db_wisata';                                     
+$host = "localhost";
+$user = "root";
+$pass = ""; 
+$db   = "db_wisata";
 
-// 2. Inisialisasi mysqli
-$koneksi = mysqli_init();
+$koneksi = mysqli_connect($host, $user, $pass, $db);
 
-// 3. Pengaturan SSL (Wajib untuk TiDB Cloud)
-mysqli_ssl_set($koneksi, NULL, NULL, NULL, NULL, NULL);
-
-// 4. Proses Koneksi
-$real_connect = mysqli_real_connect(
-    $koneksi, 
-    $host, 
-    $user, 
-    $pass, 
-    $db, 
-    $port, 
-    NULL, 
-    MYSQLI_CLIENT_SSL
-);
-
-// 5. Cek apakah berhasil
-if (!$real_connect) {
-    die("Koneksi ke TiDB Cloud gagal: " . mysqli_connect_error());
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
 }
-
-
 ?>
